29768f56705b2586826621ab982daba43e4124eb
Vishnu-Krishnakumar <vkrish3@wgu.edu>
2025-08-14 15:33:46 +0530

    changed to regex for path

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use("/posts",postRoutes);
 app.use("/comments",commentRoutes);
 app.use(express.static(path.join(__dirname, "client", "dist")));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
